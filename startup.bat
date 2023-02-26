f040aedeccdba6751a7fef161e0414c486f39461
EnPraVa <56026581+enprava@users.noreply.github.com>
2023-02-26 12:30:42 +0100

    Delete startup.bat

--- startup.bat
+++
@@ -1,7 +0,0 @@
-. venv/Scripts/activate
-pip install -r bugalink-backend/requirements.txt
-python bugalink-backend/manage.py makemigrations
-python bugalink-backend/manage.py migrate
-python bugalink-backend/manage.py runserver
-
-

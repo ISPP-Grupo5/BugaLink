3211c2051dcecfad6274b0271937d2c4454c45c5
enprava <epradavazquez@gmail.com>
2023-02-26 13:21:48 +0100

    Integracion de ambos repos

--- bugalink-backend/BugaLink/settings.py
+++ bugalink-backend/BugaLink/settings.py
@@ -9,22 +9,23 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
 from pathlib import Path
-#import environ
-
+from dotenv import dotenv_values
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+env = dotenv_values(".env")
+
 SECRET_KEY = 'django-insecure-br8yvhx^^w#x0e3i03qy($-^q49(xk-9uhf^=vj8igoa-8g#75'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = env['DEBUG']
 
 ALLOWED_HOSTS = []
 
@@ -78,12 +79,12 @@ WSGI_APPLICATION = 'BugaLink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bugalink',
-        'USER': 'buga',
-        'PASSWORD': 'bugalink',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': env['ENGINE'],
+        'NAME': env['NAME'],
+        'USER': env['USER'],
+        'PASSWORD': env['PASSWORD'],
+        'HOST': env['HOST'],
+        'PORT': env['PORT'],
     }
 }
 

61f6f472ba2473ae99147163cf01652e688e6db2
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2023-03-06 11:46:16 +0100

    fix: Corrections and comments of model

--- bugalink-backend/trips/models.py
+++ bugalink-backend/trips/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 from enum import Enum
 
-
 # Create your models here.
 class LiftStatus(Enum):
     Pending_start = 'Pending start'
@@ -13,10 +12,6 @@ class LiftStatus(Enum):
     @classmethod
     def choices(cls):
         return [(key.value, key.name) for key in cls]
-
-
-from django.db import models
-
 
 class Coord(models.Field):
     def __init__(self, *args, **kwargs):
@@ -54,19 +49,17 @@ class Passenger(models.Model):
     photo = models.FileField(null=True)
     verified = models.BooleanField(default=False)
 
-
 class Driver(models.Model):
     passenger = models.OneToOneField(Passenger, primary_key=True, on_delete=models.CASCADE)
     preferences = models.CharField(max_length=2048)
     biography = models.CharField(max_length=256)
-    has_driver_license = models.BooleanField()
-    has_sworn_declaration = models.BooleanField()
-    entry_date = models.DateField
+    has_driver_license = models.BooleanField(default=False)
+    has_sworn_declaration = models.BooleanField(default=False)
+    entry_date = models.DateField()
     sworn_declaration = models.FileField()
     driver_license = models.FileField()
     dni_front = models.FileField()
     dni_back = models.FileField()
-
 
 class Rating(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
@@ -74,38 +67,40 @@ class Rating(models.Model):
     value = models.FloatField(default=1, validators=[MinValueValidator(1.0), MaxValueValidator(5.0)])
     comment = models.CharField(max_length=1024)
 
-
 class Vehicle(models.Model):
     driver = models.ManyToManyField(Driver)
     model = models.CharField(max_length=256)
-    registration = models.CharField(max_length=256)
+    registration = models.CharField(max_length=256) # que es esto ¿model?
     has_insurance = models.BooleanField(max_length=256)
     insurance = models.FileField()
 
+#Posible creacion de entidad Default_Driver_Routine con 
+#default_vehicle
+#default_num_seats
+#default_start_location
+#default_end_location
 
 class DriverRoutine(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
     default_vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='default_vehicle')
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name='vehicle')
     default_num_seats = models.IntegerField()
-    start_date = models.TimeField()
+    start_date = models.TimeField() # models.DateTimeField() en que quedamos
     end_date = models.TimeField()
     start_location = Coord(null=True)
     end_location = Coord(null=True)
     frecuency = models.CharField(max_length=256)
     one_lift = models.BooleanField(default=False)
 
-
 class Lift(models.Model):
-    driver_routine = models.ManyToManyField(DriverRoutine)
+    driver_routine = models.ManyToManyField(DriverRoutine)  # ManytoMany?
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
-    default_num_seats = models.IntegerField()
+    default_num_seats = models.IntegerField()   # Relacionarlo con default_num_seats
     status = models.CharField(max_length=256, choices=LiftStatus.choices(), default=LiftStatus.Pending_start)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     start_location = Coord(null=True)
     end_location = Coord(null=True)
-
 
 class IndividualLift(models.Model):
     lift = models.ForeignKey(Lift, on_delete=models.CASCADE)
@@ -116,11 +111,11 @@ class IndividualLift(models.Model):
     end_location = Coord(null=True)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=256, choices=LiftStatus.choices(), default=LiftStatus.Pending_start)
-
+    #acceptation_status y su enum?
 
 class TravellerRoutine(models.Model):
     passenger = models.ForeignKey(Passenger, on_delete=models.CASCADE)
-    start_date = models.TimeField()
+    start_date = models.TimeField() # en que quedamos en TimeField o dateTimeField?
     end_date = models.TimeField()
     start_place = models.CharField(max_length=256)
     end_place = models.CharField(max_length=256)
@@ -128,28 +123,24 @@ class TravellerRoutine(models.Model):
     time_diff_before = models.DurationField()
     time_diff_after = models.DurationField()
 
-
 class CreditCard(models.Model):
     user = models.ForeignKey(Passenger, on_delete=models.CASCADE)
     card = models.CharField(max_length=32)
     CVC = models.CharField(max_length=16)
     expiration_date = models.DateField()
 
-
 class Paypal(models.Model):
-    mail = models.CharField(max_length=256, primary_key=True)
+    mail = models.CharField(max_length=256, primary_key=True)   # Posible problema con multiples users, añadir id como en modelo
     password = models.CharField(max_length=256)
     user = models.ForeignKey(Passenger, on_delete=models.CASCADE)
-
 
 class FavDirection(models.Model):
     user = models.ForeignKey(Passenger, on_delete=models.CASCADE)
     name = models.CharField(max_length=256)
-    direction = models.CharField(max_length=256)
-    city = models.CharField(max_length=256)
-    start_location = Coord(null=True)
-    cp = models.CharField(max_length=256)
-
+    direction = models.CharField(max_length=256) # O esto
+    city = models.CharField(max_length=256) # Puede llegar a ser redundante
+    start_location = Coord(null=True)   # o esto
+    cp = models.CharField(max_length=256) # Puede llegar a ser redundante/innecesario
 
 class DiscountCode(models.Model):
     code = models.CharField(max_length=256, unique=True)
@@ -157,18 +148,16 @@ class DiscountCode(models.Model):
     lifts = models.IntegerField()
     start_date = models.DateField()
     end_date = models.DateField()
-    active = models.BooleanField()
-
+    active = models.BooleanField()  
 
 class IndividualDiscountCode(models.Model):
     code = models.CharField(max_length=256, unique=True)
     discount_perc = models.FloatField()
     start_date = models.DateField()
     end_date = models.DateField()
-    disabled = models.BooleanField()
+    disabled = models.BooleanField()    #default=False
     initial_lifts = models.IntegerField()
     lifts_left = models.IntegerField()
-
 
 class TravellerDiscountCode(models.Model):
     discount = models.ForeignKey(DiscountCode, on_delete=models.CASCADE)

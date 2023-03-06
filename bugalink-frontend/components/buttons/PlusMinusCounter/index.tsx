9d569b38ea19692116264f13e1be54eb98ebe2fe
Andres Duran <andresdt@pm.me>
2023-03-07 00:02:07 +0100

    fix: capture default button onsubmit

--- bugalink-frontend/components/buttons/PlusMinusCounter/index.tsx
+++ bugalink-frontend/components/buttons/PlusMinusCounter/index.tsx
@@ -4,7 +4,7 @@ type Props = {
   text: string;
 };
 
-export default function PassengersCounter({
+export default function PlusMinusCounter({
   onMinusClick,
   onPlusClick,
   text,
@@ -12,6 +12,9 @@ export default function PassengersCounter({
   return (
     <div className="relative w-full text-center bg-base font-semibold p-1 rounded-xl flex justify-between items-center">
       <button
+        onClickCapture={(e) => {
+          e.preventDefault();
+        }}
         className="w-10 h-10 bg-white rounded-lg text-xl flex items-center justify-center"
         onClick={onMinusClick}
       >
@@ -19,6 +22,9 @@ export default function PassengersCounter({
       </button>
       <p className="text-xl">{text}</p>
       <button
+        onClickCapture={(e) => {
+          e.preventDefault();
+        }}
         className="w-10 h-10 bg-white rounded-lg text-2xl flex items-center justify-center"
         onClick={onPlusClick}
       >

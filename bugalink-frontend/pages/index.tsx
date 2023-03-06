c208a9f3de1afa1093acfe2f38f3c7b8aead2cda
Pablo José Robledo Campa <pablorcmail@gmail.com>
2023-03-06 21:33:03 +0100

    fix: Minor style changes and better coding practices

--- bugalink-frontend/pages/index.tsx
+++ bugalink-frontend/pages/index.tsx
@@ -2,6 +2,9 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import Card from '../components/cards/Passanger';
 import DriverCard from '../components/cards/Driver';
+import Chat from '/public/icons/Vista-Principal/chat.svg';
+import House from '/public/icons/Vista-Principal/house.svg';
+import Calendar from '/public/icons/Vista-Principal/calendar.svg';
 
 export default function Home() {
   return (
@@ -9,56 +12,70 @@ export default function Home() {
       <div className='flex flex-col h-full'>
         
         <div className="flex flex-col">
-          <span className='grid grid-cols-2 mt-5'>
+          <span className='relative grid grid-cols-2 mt-0.5'>
 
-            <div className='text-center text-ellipsis mt-10'>
-              <p className='text-5xl inline-block  align-botton'> Hola, Pedro</p>
+            <div className='text-center text-ellipsis mt-12 -ml-10'>
+              <p className='text-2xl inline-block align-botton font-semibold'> Hola, Pedro</p>
             </div>
             
-            <div className='text-center rounded-full'>
-              <img src="/icons/Vista-Principal/hombre.png" className='object-scale-down h-40 w-40 mx-auto scale-90'/> {/*TODO Añadir el icono en concreto*/}
+            <div className='absolute text-center rounded-full -right-3 -top-4 w-25 h-25 '>
+              <img src="/icons/Vista-Principal/hombre.png" className='object-scale-down h-40 w-40 mx-auto scale-50'/> {/*TODO Añadir el icono en concreto*/}
             </div>
 
           </span>
         </div>
 
-        <div className='flex flex-col'>
-          <span className='grid grid-cols-3  text-center items-center pt-3'>
+        <div className='flex  py-10'>
+          <span className='grid grid-cols-3 justify-items-center w-full'>
             
-              <a href='#' className='bg-base'>
-                <img src='/icons/Vista-Principal/calendar.svg' className='object-scale-down h-35 w-35 mx-auto bg-white rounded-xl' />{/*TODO Añadir el icono en concreto*/}
-                <p className='text-lg text-center'>Horarios</p>
-              </a>
-
-              <a href='#' className='bg-base'>
-                <img src='/icons/Vista-Principal/chat.svg' className='object-scale-down h-35 w-35 mx-auto bg-white  rounded-xl pb-0' /> {/*TODO Añadir el icono en concreto*/}
-                <p className='text-lg text-center'>Chats</p>
-              </a>
-
-              <a href='#' className='bg-base'>
-                <img src='/icons/Vista-Principal/house.svg' className='object-scale-down h-35 w-35 mx-auto bg-white rounded-xl' />{/*TODO Añadir el icono en concreto*/}
-                <p className='text-lg text-center'>Direcciones</p>
-              </a>
+            <Link href='#' className='bg-base' >
+              <div className=''>
+                <Calendar className='bg-white rounded-xl '/>
+              </div>
               
-            
+              <p className='text-lg text-center font-bold'>Horarios</p>
+            </Link>
+
+            <Link href='#' className='bg-base'>
+              <div className='relative content-center'>
+                <Chat className='bg-white rounded-xl '/>
+                <p className='absolute bg-lightRed w-7 h-7 rounded-full text-white text-center  -left-2 -top-2'>3</p>
+              </div>
+
+              <p className='bg-base text-lg text-center font-bold'>Chats</p>
+            </Link>
+
+            <Link href='#'className='bg-base'>
+              <div className=''>
+                <House className='bg-white rounded-xl '/>
+              </div>
+              
+              <p className='bg-base text-lg text-center font-bold'>Dirrecciones</p>
+            </Link>
           </span>
+          
+
         </div>
 
         <div className='relative' data-carousel="static">
           
-          <span className='grid grid-cols-2 mt-5'>
-              <p className='text-lg text-left ml-8'>Próximos viajes</p>
+          <span className='grid grid-cols-2 -mt-4'>
+              <p className='text-xl text-left ml-5 font-bold'>Próximos viajes</p>
               <a href='#'>
-                <p className='text-lg text-right mr-8 text-turquoise'>Historial</p>
+                <p className='text-xl text-right mr-8 text-turquoise font-bold'>Historial</p>
               </a>
           </span>
           
           <div className="relative w-full flex -space-x-7 snap-x snap-mandatory overflow-x-auto">
+                <Card />  
                 <Card /> 
-                <DriverCard /> 
-                <Card /> 
-                <DriverCard /> 
                 <Card />
+          </div>
+
+          <div className="relative w-full flex -space-x-7 snap-x snap-mandatory overflow-x-auto mt-2"> 
+                <DriverCard /> 
+                <DriverCard /> 
+                <DriverCard /> 
 
           </div>
 
--- bugalink-frontend/tailwind.config.js
+++ bugalink-frontend/tailwind.config.js
@@ -12,12 +12,15 @@ module.exports = {
     colors: {
       base: '#f1f1f1',
       black: '#000000',
+      lightGray: '#d7d7d7',
       gray: '#696969', // nice
       white: '#ffffff',
       red: '#d00000',
       'red-dark': '#da0000',
       'red-button': '#ff7e7e',
+      lightRed: '#FF6363',
       turquoise: '#38a3a5',
+      lightTurquoise: "#7cc3c4",
       green: '#75a538',
       yellow: '#f9c200',
     },

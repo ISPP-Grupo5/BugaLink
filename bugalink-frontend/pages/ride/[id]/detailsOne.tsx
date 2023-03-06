f971e1728ece738880cb8a86c6d6735c8ae4e94d
Andres Duran <andresdt@pm.me>
2023-03-06 23:24:46 +0100

    Merge branch 'develop'

---
+++ bugalink-frontend/pages/ride/[id]/detailsOne.tsx
@@ -0,0 +1,113 @@
+import Link from 'next/link';
+import BackButtonWithHeader from '../../../components/buttons/BackWithHeader';
+import CTAButton from '../../../components/buttons/CTA';
+import Chat from '/public/assets/chat.svg';
+import TargetPin from '/public/assets/map-mark.svg';
+import SourcePin from '/public/assets/source-pin.svg';
+
+export default function DetailsOne() {
+  return (
+    <div className="flex justify-center h-screen bg-base">
+      <BackButtonWithHeader text="Detalles del viaje" />
+      <div className="w-11/12 bg-white pb-44">
+        {/* Profile header */}
+        <div className="flex flex-row items-center justify-between px-5 py-2 pt-20">
+          <div className="flex flex-row items-center">
+            <img
+              src="/assets/mocks/profile1.png"
+              className="w-11 h-11 rounded-full"
+            />
+            <div className="flex flex-col ml-3">
+              <p className="text-lg font-bold leading-normal">Jesús Marchena</p>
+              <p className="text-xs font-normal">⭐ 4.8 - 14 valoraciones</p>
+            </div>
+            <div className="flex flex-col ml-5">
+              <button className="rounded-full w-7 h-7 flex items-center justify-center border border-turquoise">
+                <Chat className="w-3 h-3" />
+              </button>
+            </div>
+            <div className="flex flex-col ml-3">
+              <button className="rounded-full w-20 h-7 flex items-center justify-center border border-turquoise">
+                <p className="text-xs font-bold text-turquoise">Ver perfil</p>
+              </button>
+            </div>
+          </div>
+        </div>
+        {/* Source and target destinations */}
+        <div className="grid grid-cols-2 px-5 py-2 gap-2">
+          <div>
+            <p className="text-xs font-normal">Origen</p>
+            <p className="text-xs font-bold">
+              <SourcePin className="inline mr-2" />
+              Escuela Técnica Superior de Ingeniería Informática (ETSII), 41002
+              Sevilla
+            </p>
+          </div>
+          <div>
+            <p className="text-xs font-normal">Destino</p>
+            <p className="text-xs font-bold">
+              <TargetPin className="inline mr-2" />
+              Avenida de Andalucía, 35, Dos Hermanas, 41002 Sevilla
+            </p>
+          </div>
+        </div>
+        {/* Map preview */}
+        <Link href="/ride/V1StGXR8_Z5jdHi6B-myT/map">
+          <div className="flex flex-row items-center justify-between px-5 py-2">
+            <img
+              src="/assets/mocks/map.png"
+              className="w-full h-48 object-cover rounded-lg"
+            />
+          </div>
+        </Link>
+        {/* Details */}
+        <div className="px-5 py-2">
+          <p className="text-xs font-normal">Fecha y hora</p>
+          <p className="text-xs font-medium text-justify">
+            📅 Todos los viernes a las 21:00
+          </p>
+        </div>
+        <div className="px-5 py-2">
+          <p className="text-xs font-normal">Sobre el conductor</p>
+          <p className="text-xs font-medium text-justify">
+            🗣 Prefiero hablar durante el viaje
+          </p>
+          <p className="text-xs font-medium text-justify">
+            🎶 Prefiero ir escuchando música
+          </p>
+          <p className="text-xs font-medium text-justify">🐾 Acepto mascotas</p>
+          <p className="text-xs font-medium text-justify">
+            🚭 No fumar en el coche
+          </p>
+        </div>
+        <div className="px-5 py-2">
+          <p className="text-xs font-normal">Nota del condutor</p>
+          <p className="text-xs font-medium text-justify">
+            ✏ También puedo recoger pasajeros en otro punto si me pilla de
+            camino. Mejor pregúntame por chat antes de reservar asiento
+          </p>
+        </div>
+      </div>
+      {/* Trip request */}
+      <div className="w-full fixed bottom-0 left-0 bg-white rounded-t-xl shadow-t-lg">
+        <div className="flex flex-row items-center justify-between px-5 py-2">
+          <div className="flex flex-col">
+            <p className="text-xs font-normal">Tipo de viaje</p>
+            <p className="text-xl font-bold">Recurrente</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-xs font-normal">Precio por asiento</p>
+            <p className="text-xl font-bold">2,00€</p>
+          </div>
+          <div className="flex flex-col">
+            <p className="text-xs font-normal">Plazas libres</p>
+            <p className="text-xl font-bold">2</p>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <CTAButton className="my-6 w-11/12" text="SOLICITAR" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ bugalink-frontend/pages/ride/[id]/detailsTwo.tsx
@@ -0,0 +1,115 @@
+import Link from 'next/link';
+import BackButtonWithHeader from '../../../components/buttons/BackWithHeader';
+import Chat from '/public/assets/chat.svg';
+import SourcePin from '/public/assets/source-pin.svg';
+import TargetPin from '/public/assets/map-mark.svg';
+import Dots from '/public/assets/dots.svg';
+
+export default function DetailsOne() {
+  return (
+    <div className="flex flex-col h-full">
+      <BackButtonWithHeader text="Detalles del viaje" />
+      {/* Map preview */}
+      <Link href="/ride/V1StGXR8_Z5jdHi6B-myT/map">
+        <div className="flex flex-row items-center justify-between px-5 py-2">
+          <img
+            src="/assets/mocks/map.png"
+            className="w-full h-64 object-cover rounded-lg"
+          />
+        </div>
+      </Link>
+      {/* Details */}
+      <div className="px-5 py-2">
+        <p className="text-xs font-lato">Viernes 16 de febrero de 2023</p>
+      </div>
+      <div className="grid justify-items-center items-center grid-cols-6 grid-rows-3 px-5 py-2">
+        <span className="font-lato font-bold text-xs">21:00</span>
+        <SourcePin />
+        <span className="col-span-4 font-lato text-xs">
+          Escuela Técnica Superior de Ingeniería Informática (ETSII), 41002
+          Sevilla
+        </span>
+
+        <div></div>
+        <Dots />
+        <hr className="col-span-4 mt-1 mb-1 w-52 text-border-color" />
+
+        <span className="font-lato font-bold text-xs">21:15</span>
+        <TargetPin />
+        <span className="col-span-4 font-lato text-xs">
+          Avenida de Andalucía, 35, Dos Hermanas, 41002 Sevilla
+        </span>
+      </div>
+
+      <div className="grid justify-items-center">
+        <hr className="mt-4 mb-3 w-80 text-border-color" />
+      </div>
+
+      {/* Seats selector */}
+      <div className="grid grid-rows-2 justify-items-center items-center mb-3">
+        <span className="font-lato font-semibold text-sm ml-4">
+          ¿Cuántas plazas quieres reservar?
+        </span>
+        <div className="grid grid-cols-6 bg-base rounded-md h-11 w-4/5 justify-items-center items-center">
+          <div className="grid justify-items-center content-center rounded-md bg-white w-8 h-8">
+            <span className="font-lato font-bold text-2xl">-</span>
+          </div>
+          <div className="col-span-4">
+            <span className="font-lato font-bold text-2xl">2</span>
+          </div>
+          <div className="grid justify-items-center content-center rounded-md bg-white w-8 h-8">
+            <span className="font-lato font-bold text-2xl">+</span>
+          </div>
+        </div>
+      </div>
+      <div className="grid justify-items-center">
+        <hr className="mt-4 mb-4 w-80 text-border-color" />
+      </div>
+
+      {/* Profile header */}
+      <div className="flex flex-row items-center justify-between px-5 py-2">
+        <div className="flex flex-row items-center">
+          <img
+            src="/assets/mocks/profile1.png"
+            className="w-11 h-11 rounded-full"
+          />
+          <div className="flex flex-col ml-3">
+            <p className="text-lg font-bold">Jesús Marchena</p>
+            <p className="text-xs font-normal">⭐ 4.8 - 14 valoraciones</p>
+          </div>
+          <div className="flex flex-col ml-5">
+            <button className="rounded-full w-7 h-7 flex items-center justify-center border border-turquoise">
+              <Chat className="w-3 h-3" />
+            </button>
+          </div>
+          <div className="flex flex-col ml-3">
+            <button className="rounded-full w-20 h-7 flex items-center justify-center border border-turquoise">
+              <p className="text-xs font-bold text-turquoise">Ver perfil</p>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-row ml-5">
+        <p className="font-lato text-dark-turquoise font-bold">
+          Añade una nota al conductor
+        </p>
+      </div>
+
+      {/* Trip request */}
+      <div className="w-full mt-3 mb-2 left-0 bg-white rounded-t-lg">
+        <div className="grid justify-items-center">
+          <hr className="mt-4 mb-4 w-80 text-border-color" />
+        </div>
+        <div className="grid grid-cols-3 items-center justify-items-center">
+          <div className="flex flex-col">
+            <p className="text-xs font-normal">Precio total</p>
+            <p className="text-xl font-bold">4,00€</p>
+          </div>
+          <button className="grid col-span-2 rounded-full w-11/12 h-12 items-center justify-center bg-turquoise">
+            <p className="text-xl font-bold text-white">PAGAR</p>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}

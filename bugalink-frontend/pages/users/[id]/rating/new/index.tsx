2acbb2fcb170cef0b0e740bdef94871ceafd2f2f
Pablo José Robledo Campa <pablorcmail@gmail.com>
2023-03-07 11:19:48 +0100

    fix: Back Button

--- bugalink-frontend/pages/users/[id]/rating/new/index.tsx
+++ bugalink-frontend/pages/users/[id]/rating/new/index.tsx
@@ -1,21 +1,16 @@
 import Link from 'next/link';
-import { BackButtonText } from '../../../../../components/buttons/Back/index';
+import BackButton from '../../../../../components/buttons/Back';
 // import Chat from '/public/assets/chat.svg';
 // import SourcePin from '/public/assets/source-pin.svg';
 // import TargetPin from '/public/assets/map-mark.svg';
 // import CTAButton from '../../../components/buttons/CTA';
 import React, { useState } from 'react';
 
-
-
-
 export default function Rating() {
-
   const [rating, setRating] = useState(0);
   const [button1Selected, setButton1Selected] = useState(false);
   const [button2Selected, setButton2Selected] = useState(false);
   const [button3Selected, setButton3Selected] = useState(false);
-
 
   const handleRating = (selectedRating) => {
     setRating(selectedRating);
@@ -34,65 +29,95 @@ export default function Rating() {
   };
 
   const handleSubmit = () => {
-    // send rating data to server 
+    // send rating data to server
     console.log('Submitting rating:', rating);
     console.log(`Button 1 selected: ${button1Selected}`);
     console.log(`Button 2 selected: ${button2Selected}`);
     console.log(`Button 3 selected: ${button3Selected}`);
   };
 
-
-
   return (
     <div className="flex justify-center h-screen bg-base">
-      <BackButtonText text="¿Cómo ha ido el viaje?" />
+      <BackButton />
       <div className="w-11/12 bg-white pb-44">
         {/* Profile header */}
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }} className="flex flex-row items-center justify-between px-5 py-2 pt-20">
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '20px',
+          }}
+          className="flex flex-row items-center justify-between px-5 py-2 pt-20"
+        >
           <div className="flex flex-row items-center">
             <img
               src="/assets/mocks/avatar1.png"
               className="w-18 h-18 rounded-full "
             />
-
-
           </div>
         </div>
         {/* User */}
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }} className="grid grid-cols-2 px-5 py-2 gap-2">
-          <div className='font-bold'>
-            <p className="text-xl font-Lato">
-              Pablo Delfín López
-            </p>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '20px',
+          }}
+          className="grid grid-cols-2 px-5 py-2 gap-2"
+        >
+          <div className="font-bold">
+            <p className="text-xl font-Lato">Pablo Delfín López</p>
           </div>
-
         </div>
 
         {/* Details */}
 
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '20px' }}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            marginTop: '20px',
+          }}
+        >
           <div style={{ display: 'flex', justifyContent: 'center' }}>
             {[...Array(5)].map((star, i) => {
               const ratingValue = i + 1;
 
               return (
-                <label key={i} style={{ cursor: 'pointer', fontSize: '50px', margin: '0 5px', color: ratingValue <= rating ? '#ffc107' : 'gray' }}>
+                <label
+                  key={i}
+                  style={{
+                    cursor: 'pointer',
+                    fontSize: '50px',
+                    margin: '0 5px',
+                    color: ratingValue <= rating ? '#ffc107' : 'gray',
+                  }}
+                >
                   <input
                     type="radio"
                     name="rating"
                     value={ratingValue}
                     onClick={() => handleRating(ratingValue)}
-                    style={{ position: 'absolute', opacity: 0, pointerEvents: 'none' }}
+                    style={{
+                      position: 'absolute',
+                      opacity: 0,
+                      pointerEvents: 'none',
+                    }}
                   />
                   ★
                 </label>
               );
             })}
           </div>
-          <h2 style={{ fontSize: '14px', marginBottom: '10px' }}>No te preocupes, las valoraciones son anónimas</h2>
+          <h2 style={{ fontSize: '14px', marginBottom: '10px' }}>
+            No te preocupes, las valoraciones son anónimas
+          </h2>
 
           <div>
-            <div style= {{ display : 'flex', flexDirection: 'row'}}>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
               <button
                 style={{
                   borderRadius: '50%',
@@ -109,7 +134,11 @@ export default function Rating() {
                 }}
                 onClick={handleButton1Click}
               >
-                <img style={{ margin: 'auto' }} src="/assets/mocks/rueda.png" alt="Button 1" />
+                <img
+                  style={{ margin: 'auto' }}
+                  src="/assets/mocks/rueda.png"
+                  alt="Button 1"
+                />
               </button>
               <button
                 style={{
@@ -127,7 +156,11 @@ export default function Rating() {
                 }}
                 onClick={handleButton2Click}
               >
-                <img style={{ margin: 'auto' }} src="/assets/mocks/cara.png" alt="Button 2" />
+                <img
+                  style={{ margin: 'auto' }}
+                  src="/assets/mocks/cara.png"
+                  alt="Button 2"
+                />
               </button>
               <button
                 style={{
@@ -145,13 +178,14 @@ export default function Rating() {
                 }}
                 onClick={handleButton3Click}
               >
-                <img style={{ margin: 'auto' }} src="/assets/mocks/mano.png" alt="Button 3" />
+                <img
+                  style={{ margin: 'auto' }}
+                  src="/assets/mocks/mano.png"
+                  alt="Button 3"
+                />
               </button>
             </div>
           </div>
-
-
-
 
           <button
             style={{
@@ -172,6 +206,5 @@ export default function Rating() {
         </div>
       </div>
     </div>
-
   );
 }

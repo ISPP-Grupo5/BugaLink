d17c1dfe1424f9c1cd7fdf439539794f84edcb8f
Abraham Cobelo Galindo <62113728+Abrcobgal@users.noreply.github.com>
2023-03-08 15:55:49 +0100

    Delete serializers.py

--- bugalink-backend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from bugalinkapp.models import User
-
-class main_page_serializer(serializers.ModelSerializer):
-
-    class Meta:
-    
-        model = User
-        fields = ['idUser']

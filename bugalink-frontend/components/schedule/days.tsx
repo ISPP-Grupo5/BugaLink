637fcaf4f4f01508b924cb0a8653c0026e157eaa
Andres Duran <andresdt@pm.me>
2023-03-10 23:43:38 +0100

    refactor: remove unused components

--- bugalink-frontend/components/schedule/days.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function MyDay({ day }) {
-  return (
-    <div>
-      <p className="font-lato font-semibold text-xl text-[#2E9B95]">{day}</p>
-    </div>
-  );
-}

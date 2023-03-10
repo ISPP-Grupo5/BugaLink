3a767a96fc38536aa5b5a3aec210d29d225467ad
vicsotber <victor.soto.berenguer123@gmail.com>
2023-03-11 00:45:19 +0100

    feat: routines related views prepared for pilots
    All the related views to the routines has been prepared for user pilots

--- bugalink-frontend/pages/index.tsx
+++ bugalink-frontend/pages/index.tsx
@@ -92,12 +92,6 @@ export default function Home() {
           <Link href="/ride/V1StGXR8_Z5jdHi6B-myT/detailsTwo">
             Detalles viaje (2)
           </Link>
-          <Link href="/users/qyahXxJc/routines/passenger/new">
-            Crear rutina (pasajero)
-          </Link>
-          <Link href="/users/qyahXxJc/routines/driver/new">
-            Crear rutina (conductor)
-          </Link>
           <Link href="/recommendations">Recomendaciones</Link>
         </div>
       </div>

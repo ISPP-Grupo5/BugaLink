3c3b1e9c1bf24e260b7db60c62b58294ada94ffb
Álvaro Vázquez Ortiz <80245816+alvvazort@users.noreply.github.com>
2023-03-11 12:06:59 +0100

    feat: App engine working with some issues

--- bugalink-backend/BugaLink/settings.py
+++ bugalink-backend/BugaLink/settings.py
@@ -14,7 +14,7 @@ from urllib.parse import urlparse
 from dotenv import dotenv_values
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/

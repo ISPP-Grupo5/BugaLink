e83666094b22dc874f0703519dab93673fc1c6c0
Carmen <pr.carmengalvan@gmail.com>
2023-03-11 15:45:04 +0100

    fix: removed unused code

--- bugalink-backend/bugalinkapp/urls.py
+++ bugalink-backend/bugalinkapp/urls.py
@@ -7,7 +7,5 @@ router = routers.DefaultRouter()
 router.register(r'users/(?P<user_id>\d+)/reviews', RatingViewSet, basename='review')
 
 urlpatterns = [
-    path('api/users/<int:user_id>/reviews/', include(router.urls)),
-    path('api/users/<int:user_id>/reviews/', include(router.urls)),
     path('', include(router.urls)),
 ]

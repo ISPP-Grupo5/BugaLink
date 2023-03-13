f83157dcca8a889d66bae7f03719ace66fbd1f2b
Álvaro Vázquez Ortiz <80245816+alvvazort@users.noreply.github.com>
2023-03-13 13:46:56 +0100

    feat: static files

--- bugalink-backend/BugaLink/settings.py
+++ bugalink-backend/BugaLink/settings.py
@@ -160,8 +160,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = "static"
+STATIC_URL = "/static/"
+STATICFILES_DIRS = []
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

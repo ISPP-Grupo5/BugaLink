f86cb19bc3e94f0dbed16328c280507de47adca1
Ignacio Planas <ignacioplanas2001@gmail.com>
2023-03-17 15:32:44 +0100

    fix(end): Eliminar bad smell sonarcloud

--- bugalink-backend/bugalinkapp/urls.py
+++ bugalink-backend/bugalinkapp/urls.py
@@ -1,21 +1,21 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import *
+import views
 
 router = routers.DefaultRouter()
-router.register(r'users/(?P<user_id>\d+)/reviews', RatingViewSet, basename='review')
+router.register(r'users/(?P<user_id>\d+)/reviews', views.RatingViewSet, basename='review')
 
 urlpatterns = [
     # path('', include(router.urls)),
-    path('users/<int:id>', users.as_view()),
-    path('pendingindividualrides/', pendingIndividualRide.as_view()),
-    path('cancelledindividualrides/', cancelledIndividualRide.as_view()),
-    path('acceptedindividualrides/', acceptedIndividualRide.as_view()),
-    path('routineFilter', routineRecommendation.as_view()),
-    path('individualRides', individualRides.as_view()),
-    path('rides/<int:pk>', Rides.as_view()),
-    path('users/<int:pk>/passenger-routines', PassengerRoutineList.as_view()),
-    path('users/<int:pk>/driver-routines', DriverRoutineList.as_view()),
-    path('users/<int:user_id>/passenger-routines/<int:routine_id>', PassengerRoutineDelete.as_view()),
-    path('users/<int:user_id>/driver-routines/<int:routine_id>', PassengerRoutineDelete.as_view())
+    path('users/<int:id>', views.users.as_view()),
+    path('pendingindividualrides/', views.pendingIndividualRide.as_view()),
+    path('cancelledindividualrides/', views.cancelledIndividualRide.as_view()),
+    path('acceptedindividualrides/', views.acceptedIndividualRide.as_view()),
+    path('routineFilter', views.routineRecommendation.as_view()),
+    path('individualRides', views.individualRides.as_view()),
+    path('rides/<int:pk>', views.Rides.as_view()),
+    path('users/<int:pk>/passenger-routines', views.PassengerRoutineList.as_view()),
+    path('users/<int:pk>/driver-routines', views.DriverRoutineList.as_view()),
+    path('users/<int:user_id>/passenger-routines/<int:routine_id>', views.PassengerRoutineDelete.as_view()),
+    path('users/<int:user_id>/driver-routines/<int:routine_id>', views.PassengerRoutineDelete.as_view())
 ]

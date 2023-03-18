e0f20b9ac9ae716d79e03e0f8da56d4498991547
vicsotber <victor.soto.berenguer123@gmail.com>
2023-03-18 20:18:11 +0100

    fix: improved profile view with my wallet option

--- bugalink-frontend/components/cards/profile/index.tsx
+++ bugalink-frontend/components/cards/profile/index.tsx
@@ -2,24 +2,30 @@ import Edit from 'public/assets/edit.svg';
 import Address from 'public/assets/address.svg';
 import Settings from 'public/assets/settings.svg';
 import Arrow from 'public/assets/arrow-right.svg';
+import Wallet from 'public/assets/wallet.svg';
 
 export default function ProfileItems() {
   return (
-    <div className="divide-light-gray divide-y w-11/12">
+    <div className="w-11/12 divide-y divide-light-gray">
       <div className="flex items-center justify-center py-4">
         <Edit />
         <span className="-ml-12 w-52 font-lato">Editar perfil</span>
-        <Arrow />
+        <Arrow className="text-white" />
       </div>
       <div className="flex items-center justify-center py-4">
-        <Address className="bg-white" />
+        <Address className="text-white" />
         <span className="-ml-12 w-52 font-lato">Direcciones</span>
-        <Arrow />
+        <Arrow className="text-white" />
+      </div>
+      <div className="flex items-center justify-center py-4">
+        <Wallet className="text-white" />
+        <span className="-ml-12 w-52 font-lato">Mi Monedero</span>
+        <Arrow className="text-white" />
       </div>
       <div className="flex items-center justify-center py-4">
         <Settings />
         <span className="-ml-12 w-52 font-lato">Ajustes</span>
-        <Arrow />
+        <Arrow className="text-white" />
       </div>
     </div>
   );

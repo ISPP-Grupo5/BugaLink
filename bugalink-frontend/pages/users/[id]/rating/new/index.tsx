7b19806c666e31ee8ded55eac1d89106bf656d8d
Javier Vázquez <javivaz2001@gmail.com>
2023-03-19 18:09:01 +0100

    Rating view done

--- bugalink-frontend/pages/users/[id]/rating/new/index.tsx
+++ bugalink-frontend/pages/users/[id]/rating/new/index.tsx
@@ -1,252 +1,75 @@
-import { BackButton, BackButtonText } from '../../../../../components/buttons/Back';
-import React, { useState } from 'react';
-import CTAButton from '../../../../../components/buttons/CTA';
-import axios from 'axios';
+import { BackButtonText } from "@/components/buttons/Back";
+import CTAButton from "@/components/buttons/CTA";
+import RatingButton from "@/components/buttons/Ratings";
+import AnimatedLayout from "@/components/layouts/animated";
+import StarRating from "@/components/starRating";
+import axios from "@/lib/axios"
+import Link from "next/link";
+import { useState } from "react";
 
-export default function Rating() {
-  const [rating, setRating] = useState(0);
-  const [button1Selected, setButton1Selected] = useState(false);
-  const [button2Selected, setButton2Selected] = useState(false);
-  const [button3Selected, setButton3Selected] = useState(false);
+export default function RatingScreen() {
+  const [rating, setRating] = useState(3);
+  const [goodConduction, setGoodConduction] = useState(false);
+  const [friendlyDriver, setFriendlyDriver] = useState(true);
+  const [knewEachOther, setKnewEachOther] = useState(false);
   const [selectedButtonsText, setSelectedButtonsText] = useState('');
 
-  const handleRating = (selectedRating) => {
-    setRating(selectedRating);
+  //Add the values of the 3 RatingButtons to selectedButtonText depending if they are selected or not
+  
+
+
+  const driverId = 1; //TODO: change this to the id of the driver
+  const passengerId = 2; //TODO: change this to the passenger's id
+  const individualRideId = 1; //TODO: change this to the individual ride id
+  const userId = 2; //TODO: change this to the user id
+
+  const data = {
+    "rating_type": "driver",
+    "driver": driverId,
+    "passenger": passengerId,
+    "IndividualRide": individualRideId,
+    "user_id": userId,
+    "rating": rating,
+    "comment": selectedButtonsText
   };
 
-  const handleButton1Click = () => {
-    setButton1Selected(!button1Selected);
-    setSelectedButtonsText(selectedButtonsText ? `${selectedButtonsText}, Buena conducción` : 'Buena conducción');
+  const handleSubmit = async () => {
+    console.log("Rating: ", rating);
+    console.log("Good conduction: ", goodConduction);
+    console.log("Friendly driver: ", friendlyDriver);
+    console.log("Knew each other: ", knewEachOther);
 
+      // transform to asnyc await
+      const response = await axios.post('/users' + driverId + '/reviews/', data);
+      console.log(response.data);
   };
-
-  const handleButton2Click = () => {
-    setButton2Selected(!button2Selected);
-    setSelectedButtonsText(selectedButtonsText ? `${selectedButtonsText}, Conductor agradable` : 'Conductor agradable');
-
-  };
-
-  const handleButton3Click = () => {
-    setButton3Selected(!button3Selected);
-    setSelectedButtonsText(selectedButtonsText ? `${selectedButtonsText}, Ya nos conocíamos` : 'Ya nos conocíamos');
-
-  };
-
-
-  const handleSubmit = () => {
-    const driver_id = 1; //TODO: change this to the id of the driver
-    const passenger_id = 2; //TODO change this to the passenger's id
-    const individual_ride_id = 1; //TODO change this to the individual ride id
-    const user_id = 2; //TODO change this to the user id
-
-    const data = {
-      "rating_type": "driver",
-      "driver": driver_id,
-      "passenger": passenger_id,
-      "IndividualRide": individual_ride_id,
-      "user_id": user_id,
-      "rating": rating,
-      "comment": selectedButtonsText
-    };
-    setSelectedButtonsText(''); // reset the value of the comments to an empty string MAYBE IT WONT BE NEEDED WHEN IT REDIRECTS TO HOME PAGE OR OTHER PAGE
-
-    // send rating data to server
-    axios.post('http://localhost:8000/api/users/' + driver_id + '/reviews/', data)
-      .then(response => {
-        console.log(response.data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
-
-  };
-
-
-
-
-
-
-  return (
-    <div className="h-screen">
-      <BackButton className="absolute left-2 shadow-xl pr-2 py-3 bg-baseOrigin" />
-      <div className='bottom-0 z-10 flex w-full flex-col rounded-t-3xl bg-white px-10 pb-4 '>
-        {/* Profile header */}
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-
-          }}
-          className="flex flex-row items-center justify-between px-5 py-2">
-          <div className="font-bold">
-            <p style={{ fontWeight: '600', fontSize: '26px', lineHeight: '28px' }} className="text-xl font-Lato pt-20">¿Cómo ha ido el viaje?</p>
-          </div>
-          <div style={{ marginTop: '70px' }} className="flex flex-row items-center">
-
-            <img
-              src="/assets/mocks/avatar1.png"
-              className="w-18 h-18 rounded-full "
-            />
-          </div>
-        </div>
-        {/* User */}
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginTop: '20px',
-          }}
-          className="grid grid-cols-2 px-5 py-2 gap-2"
-        >
-          <div style={{ marginTop: '-20px' }} className="font-bold">
-            <p style={{ fontWeight: '700', fontSize: '18px', lineHeight: '16px' }} className="text-xl font-Lato">Pablo Delfín López</p>
-          </div>
-        </div>
-
-        {/* Details */}
-
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            marginTop: '20px',
-          }}
-        >
-          <div style={{ display: 'flex', justifyContent: 'center' }}>
-            {[...Array(5)].map((star, i) => {
-              const ratingValue = i + 1;
-
-              return (
-                <label
-                  key={i}
-                  style={{
-                    cursor: 'pointer',
-                    fontSize: '50px',
-                    margin: '0 5px',
-                    color: ratingValue <= rating ? '#ffc107' : 'gray',
-                    textShadow: '1px 1px 0 #999, -1px -1px 0 #fff, -1px 1px 0 #999, 1px -1px 0 #fff, 0px 0px 4px #444',
-                    borderRadius: '32px',
-
-
-
-                  }}
-                >
-                  <input
-                    type="radio"
-                    name="rating"
-                    value={ratingValue}
-                    onClick={() => handleRating(ratingValue)}
-                    style={{
-                      position: 'absolute',
-                      opacity: 0,
-                      pointerEvents: 'none',
-
-
-                    }}
-                  />
-                  ★
-                </label>
-              );
-            })}
-          </div>
-          <h2 style={{ fontWeight: '400', fontSize: '14px', marginBottom: '10px' }}>
-            No te preocupes, las valoraciones son anónimas
-          </h2>
-
-          <div style={{ display: 'flex' }}>
-            <div style={{ display: 'flex', flexDirection: 'row', marginTop: '70px' }}>
-              <button
-                style={{
-                  borderRadius: '50%',
-                  width: '60px',
-                  height: '60px',
-                  backgroundColor: button1Selected ? '#38A3A5' : 'white',
-                  border: '2px solid #38A3A5',
-                  cursor: 'pointer',
-                  margin: '10px',
-                  outline: 'none',
-                  display: 'flex',
-                  marginRight: '50px',
-                  justifyContent: 'center',
-                }}
-                onClick={handleButton1Click}
-              >
+  
+    return (
+        <AnimatedLayout className="flex flex-col justify-around items-center bg-white px-6 sm:px-14">
+            <BackButtonText text="¿Cómo ha ido el viaje?" />
+            <div className="flex flex-col items-center space-y-4">
                 <img
-                  style={{ margin: 'auto' }}
-                  src="/assets/mocks/rueda.png"
-                  alt="Button 1"
-                />
-              </button>
+                    src="/assets/mocks/avatar1.png"
+                    className="rounded-full "
 
-              <button
-                style={{
-                  borderRadius: '50%',
-                  width: '60px',
-                  height: '60px',
-                  backgroundColor: button2Selected ? '#38A3A5' : 'white',
-                  border: '2px solid #38A3A5',
-                  cursor: 'pointer',
-                  margin: '10px',
-                  outline: 'none',
-                  display: 'flex',
-                  justifyContent: 'center',
-                }}
-                onClick={handleButton2Click}
-              >
-                <img
-                  style={{ margin: 'auto' }}
-                  src="/assets/mocks/cara.png"
-                  alt="Button 2"
                 />
-              </button>
-              <button
-                style={{
-                  borderRadius: '50%',
-                  width: '60px',
-                  height: '60px',
-                  backgroundColor: button3Selected ? '#38A3A5' : 'white',
-                  border: '2px solid #38A3A5',
-                  cursor: 'pointer',
-                  margin: '10px',
-                  outline: 'none',
-                  display: 'flex',
-                  marginLeft: '50px',
-                  justifyContent: 'center',
-                }}
-                onClick={handleButton3Click}
-              >
-                <img
-                  style={{ margin: 'auto' }}
-                  src="/assets/mocks/mano.png"
-                  alt="Button 3"
-                />
-              </button>
-
+                <p className="font-bold text-xl">Pablo D. López</p>
             </div>
-          </div>
-          <div style={{ display: 'flex', marginTop: '5px' }}>
-            <h2 style={{ fontWeight: '420', fontSize: '14px', marginBottom: '10px', textAlign: 'center' }} >
-              Buena <br></br>conducción
-            </h2>
-            <h2 style={{ fontWeight: '420', fontSize: '14px', marginBottom: '10px', textAlign: 'center', marginLeft: '50px', marginRight: '50px' }} >
-              Conductor <br></br>agradable
-            </h2>
-            <h2 style={{ fontWeight: '420', fontSize: '14px', marginBottom: '10px', textAlign: 'center' }} >
-              Ya nos <br></br>conocíamos
-            </h2>
+            <div className="flex flex-col items-center space-y-3">
+            <StarRating value={rating} setValue={setRating}/>
+            <p className="text-sm text-center">No te preocupes, las valoraciones son anónimas</p>
+            </div>
 
-          </div>
-          <div className="flex flex-col justify-end">
-            <a href="link" style={{ color: '#DA0000', marginTop: '60px', marginBottom: '-13px', fontSize: '14px' }}>¿Has tenido algún problema? <b style={{ fontSize: '14px' }}>Háznoslo saber</b></a>
+            <div className="flex justify-between space-x-4">
+              <RatingButton text="Buena conducción" icon="🛞" selected={goodConduction} setSelected={setGoodConduction}/>
+              <RatingButton text="Conductor agradable" icon="🙂" selected={friendlyDriver} setSelected={setFriendlyDriver}/>
+              <RatingButton text="Ya nos conocíamos" icon="👋" selected={knewEachOther} setSelected={setKnewEachOther}/>
+            </div>
+            <div className="space-y-3 text-center">
+            <Link href="#" className='text-red'>¿Has tenido algún problema? <b className="text-red-dark">Háznoslo saber</b></Link>
+            <CTAButton onClick={handleSubmit} className="mt-6 w-full" text="ENVIAR" />
+            </div>
+        </AnimatedLayout>
 
-            <CTAButton onClick={handleSubmit} className="mt-6 max-w-8" text="ENVIAR" />
-          </div>
-        </div>
-      </div>
-    </div >
-  );
+    );
 }

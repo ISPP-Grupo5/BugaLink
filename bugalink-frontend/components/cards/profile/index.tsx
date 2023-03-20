5eaaa4c9590076dc43521c986933d0adfc61cd5f
vicsotber <victor.soto.berenguer123@gmail.com>
2023-03-20 09:54:03 +0100

    feat: changed entire profile view style
    The entire view has been changed in order to make it more appealing and avoid so much empty space

--- bugalink-frontend/components/cards/profile/index.tsx
+++ bugalink-frontend/components/cards/profile/index.tsx
@@ -1,32 +1,91 @@
-import Edit from 'public/assets/edit.svg';
 import Address from 'public/assets/address.svg';
-import Settings from 'public/assets/settings.svg';
-import Arrow from 'public/assets/arrow-right.svg';
+import Preferences from 'public/assets/preferences.svg';
+import Help from 'public/assets/help.svg';
 import Wallet from 'public/assets/wallet.svg';
+import Logout from 'public/assets/log-out.svg';
+import React, { useState } from 'react';
+import { Drawer } from '@mui/material';
 
 export default function ProfileItems() {
+  const [drawerPreferences, setDrawerPreferences] = useState(false);
   return (
-    <div className="w-11/12 divide-y divide-light-gray">
-      <div className="flex items-center justify-center py-4">
-        <Edit />
-        <span className="-ml-12 w-52 font-lato">Editar perfil</span>
-        <Arrow className="text-white" />
-      </div>
-      <div className="flex items-center justify-center py-4">
+    <div className="mx-auto flex w-11/12 flex-col items-center py-4 text-start">
+      <div className="flex items-center gap-x-2 py-4">
         <Address className="text-white" />
-        <span className="-ml-12 w-52 font-lato">Direcciones</span>
-        <Arrow className="text-white" />
+        <span className="w-52 font-lato">Direcciones</span>
       </div>
-      <div className="flex items-center justify-center py-4">
+      <div className="flex items-center gap-x-2 py-4">
         <Wallet className="text-white" />
-        <span className="-ml-12 w-52 font-lato">Mi Monedero</span>
-        <Arrow className="text-white" />
+        <span className="w-52 font-lato">Mi cartera</span>
       </div>
-      <div className="flex items-center justify-center py-4">
-        <Settings />
-        <span className="-ml-12 w-52 font-lato">Ajustes</span>
-        <Arrow className="text-white" />
+      <div
+        className="flex items-center gap-x-2 py-4"
+        onClick={() => setDrawerPreferences(true)}
+      >
+        <Preferences />
+        <span className="w-52 font-lato">Preferencias</span>
       </div>
+      <div className="w-11/12 py-4 text-light-gray">
+        <hr></hr>
+      </div>
+      <div className="flex items-center gap-x-2 py-4">
+        <Help />
+        <a href="mailto:soporte@bugalink.es" className=" w-52 font-lato">
+          <span>Ayuda</span>
+        </a>
+      </div>
+      <div className="flex items-center gap-x-2 py-4">
+        <Logout />
+        <span className="w-52 font-lato">Cerrar sesión</span>
+      </div>
+
+      <Drawer
+        anchor="bottom"
+        open={drawerPreferences}
+        onClose={() => setDrawerPreferences(false)}
+        SlideProps={{
+          style: {
+            minWidth: '320px',
+            maxWidth: '480px',
+            width: '100%',
+            margin: '0 auto',
+            backgroundColor: 'transparent',
+          },
+        }}
+      >
+        <div className="rounded-t-lg bg-white">
+          <div className="ml-6 mt-2">
+            <p className="font-lato text-2xl font-bold">Mis preferencias</p>
+            <div className="my-6 grid grid-cols-2 grid-rows-2 gap-y-3">
+              <div className="flex h-20 w-40 flex-col items-center rounded-lg border border-light-gray bg-white">
+                <p className="text-xl font-bold">🗣️</p>
+                <p className="text-center font-bold">
+                  Prefiero hablar durante el camino
+                </p>
+              </div>
+              <div className="flex h-20 w-40 flex-col items-center rounded-lg border border-light-gray bg-white">
+                <p className="text-xl font-bold">🔉</p>
+                <p className="text-center font-bold">
+                  Prefiero ir escuchando música
+                </p>
+              </div>
+              <div className="flex h-20 w-40 flex-col items-center rounded-lg border border-light-gray bg-light-gray">
+                <p className="text-xl font-bold">🚭</p>
+                <p className="text-center font-bold">
+                  Mi coche es libre de humos
+                </p>
+              </div>
+              <div className="flex h-20 w-40 flex-col items-center rounded-lg border border-light-gray bg-light-gray">
+                <p className="text-xl font-bold">😿</p>
+                <p className="text-center font-bold">
+                  No acepto pasajeros con mascotas
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="my-5 flex flex-col items-center"></div>
+        </div>
+      </Drawer>
     </div>
   );
 }
--- bugalink-frontend/pages/users/[id]/index.tsx
+++ bugalink-frontend/pages/users/[id]/index.tsx
@@ -1,22 +1,39 @@
 import AnimatedLayout from '../../../components/layouts/animated';
-import { BackButton } from '../../../components/buttons/Back';
+import { BackButtonText } from '../../../components/buttons/Back';
 import Avatar from 'public/assets/avatar.svg';
 import ProfileItems from '@/components/cards/profile';
 
 export default function Profile() {
   return (
     <AnimatedLayout className="bg-white">
-      <BackButton className="absolute left-2 top-2 bg-base-origin py-3 pr-2 shadow-xl" />
-      <div className="bg-background-turquoise flex flex-col -space-y-8">
-        <div className="z-10 rounded-t-3xl bg-base-origin text-center">
-          <div className="h-32 bg-base-origin"></div>
-          <div className="flex h-max flex-col items-center rounded-t-3xl bg-white py-5">
-            <Avatar className="z-10 -mt-16 h-20 w-20" />
-            <p className="py-5 text-3xl text-gray ">Pedro Pérez</p>
+      <BackButtonText
+        text="Mi perfil"
+        className="bg-base-origin py-3 shadow-xl"
+      />
+      <div className="flex flex-col -space-y-8 bg-base-origin">
+        <div className="z-10 rounded-t-3xl text-center">
+          <Avatar className="mx-auto mt-6 h-20 w-20" />
+          <p className="pt-5 text-3xl ">Pedro Pérez</p>
+          <p className="text-sm text-gray ">Editar perfil</p>
+          <div className="ml-4 w-11/12 py-4 text-light-gray">
+            <hr></hr>
+          </div>
+          <div className="mb-6 flex justify-evenly">
+            <div className="h-14 w-24 rounded-lg bg-white shadow-lg">
+              <p className="text-xl font-bold">2 años</p>
+              <p className="text-sm text-gray">Experiencia</p>
+            </div>
+            <div className="h-14 w-24 rounded-lg bg-white shadow-lg">
+              <p className="text-xl font-bold">32</p>
+              <p className="text-sm text-gray">Viajes</p>
+            </div>
+            <div className="h-14 w-24 rounded-lg bg-white shadow-lg">
+              <p className="text-xl font-bold">4.2⭐</p>
+              <p className="text-sm text-gray">Valoraciones</p>
+            </div>
+          </div>
+          <div className="flex h-max items-start rounded-t-3xl bg-white py-5">
             <ProfileItems />
-            <p className="fixed bottom-0 mb-5 mr-5 font-lato text-sm text-light-red">
-              Log out
-            </p>
           </div>
         </div>
       </div>

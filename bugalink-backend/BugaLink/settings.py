b55b6629c5d04a51646b04e48f54d941196bef25
Álvaro Vázquez Ortiz <80245816+alvvazort@users.noreply.github.com>
2023-03-25 20:28:04 +0100

    feat: logout working

--- bugalink-backend/BugaLink/settings.py
+++ bugalink-backend/BugaLink/settings.py
@@ -190,3 +190,9 @@ MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}

3f4ba8cadab043bd7ba00f05db1c6c919fcf88b6
Ignacio Planas <80254262+IgnacioPlanasTh@users.noreply.github.com>
2023-03-25 12:29:39 +0100

    feat(end): testing del endpoint
    Se ha testeado el endpoint
    /api/rides/ride_id/detail
    
    Fixes #252

--- bugalink-backend/bugalinkapp/tests.py
+++ bugalink-backend/bugalinkapp/tests.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from django.core.files.uploadedfile import SimpleUploadedFile
 import json
-import populate_db as populate
+# import populate_db as populate
 
 
 ### IMPORTANTE ###
@@ -230,6 +230,7 @@ class RatingTest(TestCase):
         url = "/api/test/users/" + str(self.user1.pk) + "/reviews/rating"
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
+
 class GetTest(TestCase):
     # Se inicializan datos para que se puedan devolver
     def setUp(self):
@@ -818,21 +819,46 @@ class IndividualRidesTest(TestCase):
 class RideDetailTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        User.objects.create(username="User1", password="My super secret pw")
-        populate.execute()
-    
-    def tearDown(self):
-        pass
-    def test_get_ride_detail(self):
-        pass
+        self.user1 = User.objects.create(username="TEST USER1", email="test1@test.es")
+        self.passenger1 = Passenger.objects.create(user=self.user1, balance=0.0)
+        self.user2 = User.objects.create(username="TEST USER2", email="test2@test.es")
+        self.passenger2 = Passenger.objects.create(user=self.user2, balance=0.0)
+        self.driver1 = Driver.objects.create(passenger=self.passenger1)
+        self.vehicle1 = Vehicle.objects.create(driver=self.driver1)
+        self.driver_routine1 = DriverRoutine.objects.create(driver=self.driver1,
+                                                            default_vehicle=self.vehicle1,default_num_seats=4,
+                                                            start_date_0=time(12,00),
+                                                            start_date_1=time(12,30),
+                                                            end_date=time(13,00),
+                                                            start_location="Virgen de Lujan 120",
+                                                            end_location="Paseo de las Delicias S/N",
+                                                            day=Days.Mon,
+                                                            one_ride=True,
+                                                            price=2.0)
+        self.passenger_routine1 = PassengerRoutine.objects.create(passenger=self.passenger1,start_time_initial=time(12,00),
+                                                                  start_time_final=time(12,30),
+                                                                  end_date=time(13,00),
+                                                                  start_location="Virgen de Lujan 120",
+                                                                  end_location="Paseo de las Delicias S/N",
+                                                                  day=Days.Mon)
 
-class RideDetailTest(TestCase):
-    def setUp(self):
-        self.client = APIClient()
-        User.objects.create(username="User1", password="My super secret pw")
-        populate.execute()
+        self.ride1 = Ride.objects.create(driver_routine=self.driver_routine1,
+                                         num_seats=self.driver_routine1.default_num_seats,
+                                          start_date = datetime(2020,4,7),
+                                           end_date =datetime(2020,4,7),
+                                           start_location = self.driver_routine1.start_location,
+                                           end_location = self.driver_routine1.end_location
+                                           )
+        self.individual_ride1 = IndividualRide.objects.create(ride=self.ride1,
+                                                              passenger=self.passenger2,
+                                                              n_seats=1,
+                                                              acceptation_status="Accepted")
     
     def tearDown(self):
         pass
     def test_get_ride_detail(self):
-        pass
+        url = "/api/rides/{}/detail".format(self.ride1.pk)
+        response = self.client.get(url)
+        data = json.loads(response.content)
+        self.assertEqual(data['available_seats'],self.ride1.num_seats - self.individual_ride1.n_seats)
+        self.assertEqual(data['recurrent'],not self.driver_routine1.one_ride)

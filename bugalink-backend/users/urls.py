df64149355cfb1f81655e1f50c459234027ef4f1
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2023-04-03 10:21:10 +0200

    feat: Endpoints "vista principal"

--- bugalink-backend/users/urls.py
+++ bugalink-backend/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from users.views import BecomeDriverView, UserTripsView, UserViewSet
+from users.views import BecomeDriverView, UserTripsView, UserTripsCountView, UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
@@ -10,4 +10,6 @@ urlpatterns = [
     path("", include(router.urls)),
     path("users/become-driver", BecomeDriverView.as_view()),
     path("users/<int:id>/trip-requests", UserTripsView.as_view()),
+    path("users/<int:id>/trip-requests/count", UserTripsCountView.as_view()),
+    path("users/<int:id>/trip/upcoming", UserTripsCountView.as_view()),
 ]

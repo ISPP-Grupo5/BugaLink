d069fbe6dcbe330975447a9177db6f52b46bf427
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2023-04-07 12:45:12 +0200

    fix: fixed test

--- bugalink-backend/transactions/tests.py
+++ bugalink-backend/transactions/tests.py
@@ -8,10 +8,11 @@ from .serializers import TransactionSerializer
 class TransactionViewSetTest(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.url = reverse('get_last_transactions', kwargs={'user_id': 1})
 
-        self.sender = User.objects.create(email="test@test.com", first_name="Semder", last_name="lastNameTest", is_passenger = True, is_driver=True)
+        self.sender = User.objects.create(email="test@test.com", first_name="Sender", last_name="lastNameTest", is_passenger = True, is_driver=True)
         self.receiver = User.objects.create(email="test@test.com", first_name="Receiver", last_name="lastNameTest", is_passenger = True, is_driver=True)
+        self.client.force_authenticate(user=self.sender)
+        self.client.force_authenticate(user=self.receiver)
 
         self.transaction1 = Transaction.objects.create(
             sender=self.sender,
@@ -36,7 +37,9 @@ class TransactionViewSetTest(TestCase):
         )
 
     def test_get_last_transactions(self):
-        response = self.client.get(self.url)
+        url = "/api/v1/users/" + str(self.sender.pk) + "/transactions/get_last_transactions/"
+        response = self.client.get(url)
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         serializer_data = TransactionSerializer([self.transaction2, self.transaction1, self.transaction3], many=True).data

b948d32029d7a783a30ab18255d3fab094576462
gonmarmar5 <80246529+gonmarmar5@users.noreply.github.com>
2023-04-08 11:29:06 +0200

    fix: corrections of Andres

--- bugalink-backend/drivers/views.py
+++ bugalink-backend/drivers/views.py
@@ -31,40 +31,37 @@ class DriverViewSet(
         try:
             user_id = kwargs["user_id"]
             user = User.objects.get(id=user_id)
-            driver = m.Driver.objects.get(user=user)
-
-            status = "Waiting validation"
-            if "sworn_declaration" in request.data:
-                driver.sworn_declaration = request.data["sworn_declaration"]
-                driver.sworn_declaration_status = status
-                
-            if "driver_license" in request.data:
-                driver.driver_license = request.data["driver_license"]
-                driver.driver_license_status = status
-            
-            if "dni_front" in request.data:
-                driver.dni_front = request.data["dni_front"]
-                driver.dni_status = status
-
-            if "dni_back" in request.data:
-                driver.dni_back = request.data["dni_back"]
-                driver.dni_status = status
-            
-            driver.save()
-
-            return Response(self.get_serializer(driver).data)
-
+        
         except User.DoesNotExist:
             return Response(
                 status=status.HTTP_400_BAD_REQUEST,
-                data={"error": "User does not exist"},
-            )
+                data={"error": "User does not exist"})
 
-        except m.Driver.DoesNotExist:
-            return Response(
-                status=status.HTTP_400_BAD_REQUEST,
-                data={"error": "Driver does not exist"},
-            )
+        try:
+            driver = m.Driver.objects.get(user=user)
+        except:
+            driver = m.Driver.objects.create(user=user)
+
+        status = "Validated"
+        if "sworn_declaration" in request.data:
+            driver.sworn_declaration = request.data["sworn_declaration"]
+            driver.sworn_declaration_status = status
+            
+        if "driver_license" in request.data:
+            driver.driver_license = request.data["driver_license"]
+            driver.driver_license_status = status
+        
+        if "dni_front" in request.data:
+            driver.dni_front = request.data["dni_front"]
+            driver.dni_status = status
+
+        if "dni_back" in request.data:
+            driver.dni_back = request.data["dni_back"]
+            driver.dni_status = status
+        
+        driver.save()
+
+        return Response(self.get_serializer(driver).data)
 
 
 #GET /drivers/<id>/preferences/

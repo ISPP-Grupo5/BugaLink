eeb182fdccd395c18aaa046e3f9b9cfc02973a74
Álvaro Vázquez Ortiz <80245816+alvvazort@users.noreply.github.com>
2023-04-10 16:02:16 +0200

    fix: ws URL

--- bugalink-frontend/hooks/useChatSocket.ts
+++ bugalink-frontend/hooks/useChatSocket.ts
@@ -7,7 +7,12 @@ export default function useChatSocket(chatId: string | number, user: User) {
     return new Promise<string>((resolve) => {
       if (user?.access && chatId) {
         // TODO: use env variable for backend url and check that it works
-        resolve(`ws://${process.env.NEXT_PUBLIC_DOMAIN_WS}/ws/chat/${chatId}/?token=${user.access}`);
+        if (process.env.NODE_ENV === 'production') {
+          resolve(`ws://app.bugalink.es/ws/chat/${chatId}/?token=${user.access}`);
+        } else {
+          resolve(`ws://localhost:8000/ws/chat/${chatId}/?token=${user.access}`);
+        }
+
       }
     });
   }, [chatId, user]);

94792fcee80f09741696b895bec2de147844f811
EnPraVa <56026581+enprava@users.noreply.github.com>
2023-04-11 21:37:07 +0200

    Delete bugalink-backend-legacy directory

--- bugalink-backend-legacy/newsletter/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class newsletter(models.Model):
-    email = models.CharField(max_length=100, unique=True)
--- bugalink-backend-legacy/newsletter/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import newsletter
-
-class newsletter_serializer(serializers.ModelSerializer):
-
-    class Meta:
-    
-        model = newsletter
-        fields = ['email']

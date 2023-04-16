f95a57a71f26467b944db55be216b9087a83d50d
Abrcobgal <abrahamconil555@gmail.com>
2023-04-16 13:39:04 +0200

    test: test trip finished added

--- bugalink-backend/trips/tests.py
+++ bugalink-backend/trips/tests.py
@@ -215,7 +215,7 @@ class GetTripRecommendationTest(TestCase):
             day_of_week="Mon",
         )
 
-        ###### Caso rutina similar pero diferente dia
+        ###### Caso rutina similar pero diferente dia (day of week)
 
         # Pasajero
         self.user_7 = User.objects.create(
@@ -296,6 +296,87 @@ class GetTripRecommendationTest(TestCase):
             day_of_week="Tue",
         )
 
+        ###### Caso rutina similar pero status incorrecto
+
+        # Pasajero
+        self.user_9 = User.objects.create(
+            email="test9@test.com",
+            first_name="Nineth User",
+            last_name="lastNameTest9",
+            is_passenger=True,
+            is_driver=False,
+        )
+        self.passenger_9 = Passenger.objects.create(user=self.user_9)
+
+        # Conductor
+        self.user_10 = User.objects.create(
+            email="test10@test.com",
+            first_name="Tenth User",
+            last_name="lastNameTest10",
+            is_passenger=True,
+            is_driver=True,
+        )
+        self.passenger_10 = Passenger.objects.create(user=self.user_10)
+        self.driver_10 = Driver.objects.create(user=self.user_10)
+
+        # Origen driver
+        self.location_origin_10 = Location.objects.create(
+            address="Mi casa", latitude=200.1231231, longitude=200.11118945
+        )
+
+        # Destino driver
+        self.location_destination_10 = Location.objects.create(
+            address="Mi casa", latitude=200.1231231, longitude=200.11118945
+        )
+
+        # Driver routine
+        self.driver_routine_10 = DriverRoutine.objects.create(
+            driver=self.driver_10,
+            price=1,
+            is_recurrent=True,
+            available_seats=1,
+            departure_time_start=datetime.datetime.now().time(),
+            departure_time_end=datetime.datetime.now().time(),
+            arrival_time=datetime.datetime.now().time(),
+            origin=self.location_origin_8,
+            destination=self.location_destination_8,
+            day_of_week="Tue",
+        )
+
+        # Trip
+        self.trip_10 = Trip.objects.create(
+            driver_routine=self.driver_routine_8,
+            departure_datetime=datetime.datetime.now() + datetime.timedelta(days=1),
+            arrival_datetime=datetime.datetime.now()
+            + datetime.timedelta(days=1, hours=1),
+            status="FINISHED",
+        )
+
+        # Origen pasajero
+        self.location_close_origin_9 = Location.objects.create(
+            address="Al lado de mi casa", latitude=200.1241231, longitude=200.11118945
+        )
+
+        # Destino pasajero
+        self.location_close_destination_9 = Location.objects.create(
+            address="Al lado de mi casa", latitude=200.1241231, longitude=200.11118945
+        )
+
+        # Passenger Routine
+        self.passenger_routine_9 = PassengerRoutine.objects.create(
+            passenger=self.passenger_9,
+            departure_time_start=(
+                datetime.datetime.now() - datetime.timedelta(hours=1)
+            ).time(),
+            departure_time_end=(
+                datetime.datetime.now() + datetime.timedelta(hours=1)
+            ).time(),
+            arrival_time=(datetime.datetime.now() + datetime.timedelta(hours=1)).time(),
+            origin=self.location_close_origin_7,
+            destination=self.location_close_destination_7,
+            day_of_week="Tue",
+        )
+
     def test_get_two_trip_recommendations(self):
         self.client.force_authenticate(user=self.user_2)
         url = "/api/v1/trips/recommendations/"
@@ -326,6 +407,13 @@ class GetTripRecommendationTest(TestCase):
 
     def test_get_trip_recommendations_wrong_day(self):
         self.client.force_authenticate(user=self.user_7)
+        url = "/api/v1/trips/recommendations/"
+        response = self.client.get(url)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 0)  # Ninguna recomendacion disponible
+
+    def test_get_trip_recommendations_trip_finished(self):
+        self.client.force_authenticate(user=self.user_9)
         url = "/api/v1/trips/recommendations/"
         response = self.client.get(url)
         data = json.loads(response.content)

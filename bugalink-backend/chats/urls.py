0bea834e192a37fe5dad43ef4486bf4b0f3c567c
Matthew Bwye Lera <73072943+MatthewBL@users.noreply.github.com>
2023-04-18 12:04:07 +0200

    fix: chat urls fixed

--- bugalink-backend/chats/urls.py
+++ bugalink-backend/chats/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("conversations/start/", views.start_convo, name="start_convo"),
     path(
         "users/<int:user_id>/conversation/",
         views.GetConversationView.as_view({"get":"get"}),

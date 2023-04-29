e0d95310396f117c8fb2da61011989f9952c7f12
Andres Duran <andresdt@pm.me>
2023-04-29 14:31:24 +0200

    feat: unify confirmation screens

--- bugalink-frontend/components/pages/confirmation/index.tsx
+++ bugalink-frontend/components/pages/confirmation/index.tsx
@@ -3,7 +3,11 @@ import Check from '/public/assets/check.svg';
 import Cross from '/public/icons/Cross.svg';
 import Link from 'next/link';
 import NEXT_ROUTES from '@/constants/nextRoutes';
-export default function ConfirmationScreen({ title = '', description = '' }) {
+export default function ConfirmationScreen({
+  title = '',
+  description = '',
+  success = true,
+}) {
   return (
     <AnimatedLayout className="bg-white">
       <Link href={NEXT_ROUTES.HOME} className="absolute top-4 right-4">
@@ -11,12 +15,23 @@ export default function ConfirmationScreen({ title = '', description = '' }) {
       </Link>
 
       <div className="flex h-full flex-col items-center justify-center text-center">
-        <span className="aspect-square rounded-full bg-green p-8 outline outline-8 outline-white">
-          <Check className="h-16 w-16 place-self-center text-white" />
-        </span>
+        {success ? <GreenCheckmark /> : <RedCross />}
+
         <h1 className="mt-8 text-xl font-bold">{title}</h1>
         <p className="text-gray">{description}</p>
       </div>
     </AnimatedLayout>
   );
 }
+
+const GreenCheckmark = () => (
+  <span className="aspect-square rounded-full bg-green p-6 outline outline-8 outline-white">
+    <Check className="h-8 w-8 place-self-center text-white" />
+  </span>
+);
+
+const RedCross = () => (
+  <span className="aspect-square rounded-full bg-red p-3 outline outline-8 outline-white">
+    <Cross className="h-14 w-14 place-self-center text-white" />
+  </span>
+);
--- bugalink-frontend/pages/trips/[id]/pay/fail/index.tsx
+++ bugalink-frontend/pages/trips/[id]/pay/fail/index.tsx
@@ -1,20 +1,11 @@
-import { BackButtonText } from '@/components/buttons/Back';
-import AnimatedLayout from '@/components/layouts/animated';
-import RedCancel from 'public/assets/cancel.svg';
+import ConfirmationScreen from '@/components/pages/confirmation';
 
 export default function PayFail() {
   return (
-    <AnimatedLayout className="justify-between flex flex-col">
-      <BackButtonText text="Pago fallido" />
-      <div className="flex h-full flex-col items-center justify-center overflow-y-scroll bg-white px-4 pb-4">
-        <RedCancel className="fill-red stroke-red" width="15vh" height="15vh" />
-        <span className="mb-5 text-center text-3xl font-bold">
-          El pago no se ha realizado correctamente
-        </span>
-        <span className="text-center text-xl">
-          Inténtalo de nuevo en unos instantes, sentimos las molestias
-        </span>
-      </div>
-    </AnimatedLayout>
+    <ConfirmationScreen
+      title="El pago no se ha realizado correctamente"
+      description="Inténtalo de nuevo en unos instantes, sentimos las molestias"
+      success={false}
+    />
   );
 }

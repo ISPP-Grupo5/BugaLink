0d87f242bfcc6d8b0c4d915626aac18915ee2c6a
Pablo José Robledo Campa <pablorcmail@gmail.com>
2023-05-15 20:32:26 +0200

    fix: Added the legal doc and remove unncesary file input

---
+++ bugalink-frontend/components/files/index.tsx
@@ -0,0 +1,16 @@
+
+
+export default function FileDownload() {
+    return (
+        <span className="flex space-x-2">
+            <p className="text-sm">Puedes descargar la Declaración jurada aquí:</p>
+            <a
+            href="/assets/files/Declaración_Jurada.pdf"
+            className="text-turquoise text-sm underline"
+            >
+            Declaración Jurada
+            </a>
+        </span>
+    )
+}
+
